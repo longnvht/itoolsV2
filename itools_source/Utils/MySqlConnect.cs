56d17d9fea2bb0494983e55ff8b5d583f999e004
longnv <longnvht@gmail.com>
2023-06-10 08:10:18 +0700

    Change Database

--- itools_source/Utils/MySqlConnect.cs
+++ itools_source/Utils/MySqlConnect.cs
@@ -18,11 +18,11 @@ namespace VinamiToolUser.Utils
 
         public static string ConnectionString()
         {
-            _strHOST = "192.168.0.50";
+            _strHOST = "localhost";
             _strPORT = "3306";
             _strDATABASE_MAME = "itooldtb";
             _strUSER_NAME = "admin";
-            _strPASSWORD = "Vinam@12345";
+            _strPASSWORD = "P@ssword";
             return ("server=" + _strHOST + ";Port=" + _strPORT + ";Database=" + _strDATABASE_MAME + ";User ID=" + _strUSER_NAME + ";Password=" + _strPASSWORD);
         }
         public static string ConnectionStringTwo()
